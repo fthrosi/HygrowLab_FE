e87dbac684b8e274dad993812ebf85471c95ce5f
joanpurba2 <joancarolus1@gmail.com>
2024-11-18 09:42:37 +0700

    Responsif page

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,17 +1,16 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
-import Sidebar from "../components/sidebar";
-import NavbarProfile from "../components/navbarProfile";
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Sidebar from '../components/sidebar';
+import NavbarProfile from '../components/navbarProfile';
 
 export default function LandingLayout() {
   return (
-    <div className="flex min-h-screen">
+    <div className="flex bg min-h-screen">
       <Sidebar />
-      <div className="w-full min-h-screen bg-off-white py-[2rem] px-[2.594rem]">
+      <div className="w-full  min-h-screen bg-off-white py-[2rem] px-[2.594rem]">
         <NavbarProfile />
         <Outlet />
       </div>
-      
     </div>
   );
 }
