9626247586fc5ae47d1784cde47a62863b35467c
joanpurba2 <joancarolus1@gmail.com>
2024-11-18 21:04:45 +0700

    coba coba

--- src/components/navbarProfile.jsx
+++ src/components/navbarProfile.jsx
@@ -1,19 +1,17 @@
-
-
 export default function NavbarProfile() {
-    return  (
-        <div className="py-[0.625rem] px-[2rem] w-full bg-white flex justify-end rounded-md">
-            <div className="flex gap-4">
-                <div className="size-[50px] rounded-full bg-off-white flex justify-center items-center">
-                    <img src="/assets/icons/settings.png" alt="" />
-                </div>
-                <div className="size-[50px] rounded-full bg-off-white flex justify-center items-center">
-                    <img src="/assets/icons/notif.png" alt="" />
-                </div>
-                <div className="size-[50px] rounded-full bg-off-white flex justify-center items-center">
-                    <img src="/assets/icons/avatar.png" alt="" className="rounded-full" />
-                </div>
-            </div>
+  return (
+    <div className="py-[0.625rem] px-[2rem] w-full bg-white flex justify-end rounded-md">
+      <div className="flex gap-4">
+        <div className="size-[50px] rounded-full bg-off-white flex justify-center items-center">
+          <img src="/assets/icons/settings.png" alt="" />
         </div>
-    )
+        <div className="size-[50px] rounded-full bg-off-white flex justify-center items-center">
+          <img src="/assets/icons/notif.png" alt="" />
+        </div>
+        <div className="size-[50px] rounded-full bg-off-white flex justify-center items-center">
+          <img src="/assets/icons/avatar.png" alt="" className="rounded-full" />
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,15 +1,72 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Sidebar from '../components/sidebar';
-import NavbarProfile from '../components/navbarProfile';
-
-export default function LandingLayout() {
+export default function Dasboard() {
+  console.log('Dasboard');
   return (
-    <div className="flex bg min-h-screen">
-      <Sidebar />
-      <div className="w-full  min-h-screen bg-off-white py-[2rem] px-[2.594rem]">
-        <NavbarProfile />
-        <Outlet />
+    <div className="w-full">
+      <p className="nunito font-bold text-[22px]  mt-9">Dasboard</p>
+      <div className="w-[330px] h-[130px] bg-white  mt-9 flex">
+        <div className="w-[70%] h-full flex items-center   ">
+          <div className="p-5">
+            <p className="text-[21px] font-extrabold nunito">5</p>
+            <p className="text-[21px] font-medium text-[rgb(3,2,41)] nunito ">
+              Jumlah Tanaman
+            </p>
+          </div>
+        </div>
+        <div className="w-[30%] h-full flex items-center p-3">
+          <img
+            className="w-full h-[90px]"
+            src="/assets/icons/Icon.png"
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="w-full mt-9  flex gap-5">
+        <div className="w-[500px] h-[300px] bg-white">
+          <div className="p-5">
+            <h1 className="nunito text-[23px] font-bold">Data Tanaman</h1>
+            <p className="nunito font-bold mt-6">Tanaman</p>
+            <hr className="w-[50%] mt-3 border border-gray-300 text-[16px]" />
+            <ol className="nunito ">
+              <li className="mb-3 mt-2">1. Bayam</li>
+              <li className="mb-3">2. Kangkung</li>
+              <li className="mb-3">3. Sawi</li>
+              <li className="mb-3">4. Pakcoy</li>
+            </ol>
+          </div>
+        </div>
+        <div className="h-[300px] w-[700px]  bg-white ">
+          <div className="p-5">
+            <h1 className="nunito text-[23px] font-bold">
+              Data Pencatatan Terbaru
+            </h1>
+            <table class="table-auto nunito w-[85%] text-start mt-3">
+              <thead className="border-b-2 border-gray-300">
+                <tr>
+                  <th className="text-start py-2">Nama Tanaman</th>
+                  <th className="text-start py-2">Jenis Tanaman</th>
+                  <th className="text-start py-2">Tanggal Mulai</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr className="border-b-2 border-gray-300">
+                  <td className="py-2">Bayam Kolam 1</td>
+                  <td className="py-2">Sawi Kolam 2</td>
+                  <td className="py-2">Kangkung kolam 3</td>
+                </tr>
+                <tr className="border-b-2 border-gray-300">
+                  <td className="py-2">Bayam</td>
+                  <td className="py-2">Sawi</td>
+                  <td className="py-2">Kangkung</td>
+                </tr>
+                <tr className="border-b-2 border-gray-300">
+                  <td className="py-2">03-09-2024</td>
+                  <td className="py-2">03-09-2024</td>
+                  <td className="py-2">03-09-2024</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
       </div>
     </div>
   );
