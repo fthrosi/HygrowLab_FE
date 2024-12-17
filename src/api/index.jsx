d05ec15ffe6fde1046fd9eab9267af47b2e4606c
kucing kalem <frosi551@gmail.com>
2024-12-17 23:20:09 +0700

    enviroment variabels

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = "http://localhost:4000";
+const BASE_URL = `${process.env.BACKEND_PUBLIC_API_URL}`;
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL, 
