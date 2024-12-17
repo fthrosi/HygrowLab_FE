098f384b0d19b4359e29edc3a1907853cd626042
kucing kalem <frosi551@gmail.com>
2024-12-17 23:47:04 +0700

    enviroment variabels

--- src/api/index.jsx
+++ src/api/index.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = `${process.env.REACT_APP_BACKEND_PUBLIC_API_URL}`;
+const BASE_URL = `${process.env.VITE_APP_BACKEND_PUBLIC_API_URL}`;
 
 const axiosInstance = axios.create({
     baseURL: BASE_URL, 
